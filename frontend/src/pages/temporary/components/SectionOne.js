2ea6bf85a5532988986410f01e292fa89dd37217
Shawn Chi <shawnchi@shawns-mbp-2.lan>
2022-05-24 16:28:24 -0700

    install styled-components

---
+++ frontend/src/pages/temporary/components/SectionOne.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+export const SectionOne = () => {
+  return (
+    <Section></Section>
+  )
+}
+
+
+const Section = styled.section`
+    width: 100%;
+    height: 50vh;
+    background: blue;
+`
