2466e30ea2d1bea3835ae49ef5a186ff5b53d425
Jiaqi <ejade2816@gmail.com>
2023-07-20 00:51:29 -0700

    cors

--- frontend/src/pages/breeder-information-collection/BreederInformationCollection.jsx
+++ frontend/src/pages/breeder-information-collection/BreederInformationCollection.jsx
@@ -87,13 +87,13 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>Cattery name</Label>
 						</Form.Label>
-						<Form.Control name="catteryName" placeholder="Cattery name" required={false} value={data["catteryName"]} onChange={handleOnChange} />
+						<Form.Control name="catteryName" placeholder="Cattery name" required={true} value={data["catteryName"]} onChange={handleOnChange} />
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Cattery owner's name</Label>
 						</Form.Label>
-						<Form.Control style={{ marginBottom: 12 }} name="catteryOwnerName1" placeholder="Owner's name 1" required={false} onChange={handleOnChange}/>
+						<Form.Control style={{ marginBottom: 12 }} name="catteryOwnerName1" placeholder="Owner's name 1" required={true} onChange={handleOnChange}/>
 						<Form.Control style={{ marginBottom: 12 }} name="catteryOwnerName2" placeholder="Owner's name 2 (Optional)" onChange={handleOnChange}/>
 						<Form.Control style={{ marginBottom: 12 }} name="catteryOwnerName3" placeholder="Owner's name 3 (Optional)" onChange={handleOnChange}/>
 					</Form.Group>
@@ -101,7 +101,7 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>Cattery address</Label>
 						</Form.Label>
-						<Form.Control style={{ marginBottom: 12 }} name="catteryAddress1" placeholder="Cattery's address 1" required={false} onChange={handleOnChange}/>
+						<Form.Control style={{ marginBottom: 12 }} name="catteryAddress1" placeholder="Cattery's address 1" required={true} onChange={handleOnChange}/>
 						<Form.Control style={{ marginBottom: 12 }} name="catteryAddress2" placeholder="Cattery's address 2 (Optional)" onChange={handleOnChange}/>
 						<Form.Control style={{ marginBottom: 12 }} name="catteryAddress3" placeholder="Cattery's address 3 (Optional)" onChange={handleOnChange}/>
 					</Form.Group>
@@ -109,7 +109,7 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>Breed</Label>
 						</Form.Label>
-						<Form.Control style={{ marginBottom: 12 }} name="catteryBreed1" placeholder="Breed 1" required={false} onChange={handleOnChange}/>
+						<Form.Control style={{ marginBottom: 12 }} name="catteryBreed1" placeholder="Breed 1" required={true} onChange={handleOnChange}/>
 						<Form.Control style={{ marginBottom: 12 }} name="catteryBreed2" placeholder="Breed 2 (Optional)" onChange={handleOnChange}/>
 						<Form.Control style={{ marginBottom: 12 }} name="catteryBreed3" placeholder="Breed 3 (Optional)" onChange={handleOnChange}/>
 					</Form.Group>
@@ -117,7 +117,7 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>Created since (MM-DD-YYYY)</Label>
 						</Form.Label>
-						<Form.Control style={{ marginBottom: 12 }} name="catteryCreatedSince" placeholder="MM-DD-YYYY" required={false} onChange={handleOnChange}/>
+						<Form.Control style={{ marginBottom: 12 }} name="catteryCreatedSince" placeholder="MM-DD-YYYY" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
@@ -132,19 +132,19 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>Breeder's name</Label>
 						</Form.Label>
-						<Form.Control name="breederName" placeholder="Breeder's name" required={false} onChange={handleOnChange}/>
+						<Form.Control name="breederName" placeholder="Breeder's name" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Breeder's email</Label>
 						</Form.Label>
-						<Form.Control name="breederEmail" placeholder="Breeder's email" required={false} onChange={handleOnChange}/>
+						<Form.Control name="breederEmail" placeholder="Breeder's email" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Breeder's phone number (555-555-5555)</Label>
 						</Form.Label>
-						<Form.Control name="breederPhone" placeholder="555-555-5555" required={false} onChange={handleOnChange}/>
+						<Form.Control name="breederPhone" placeholder="555-555-5555" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 
 					<hr />
@@ -153,19 +153,19 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>How many Sires do you have?</Label>
 						</Form.Label>
-						<Form.Control name="siresCnt" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="siresCnt" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>How many Dams do you have?</Label>
 						</Form.Label>
-						<Form.Control name="damsCnt" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="damsCnt" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>How frequently do you accquire new Sires/Dams</Label>
 						</Form.Label>
-						<Form.Control name="frequentlyAccquireNewSiresDams" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="frequentlyAccquireNewSiresDams" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label><Label>Upload Sires and Dams regisation (multiple files)</Label></Form.Label>
@@ -178,97 +178,97 @@ function BreederInformationCollection() {
 						<Form.Label>
 							<Label>How many litters of kittens do you breed each year?</Label>
 						</Form.Label>
-						<Form.Control name="littersOfKittensBreedEachYear" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="littersOfKittensBreedEachYear" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>What is the typical price range of kittens?</Label>
 						</Form.Label>
-						<Form.Control name="typicalPriceRangeOfKittens" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="typicalPriceRangeOfKittens" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>How long have you been involved in breeding?</Label>
 						</Form.Label>
-						<Form.Control name="howLongBeenInvolvedInBreeding" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="howLongBeenInvolvedInBreeding" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>How many kittens have you successfully placed in new homes? (per year?)</Label>
 						</Form.Label>
-						<Form.Control name="kittensSuccessfullyPlacedInNewHomesPerYear" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="kittensSuccessfullyPlacedInNewHomesPerYear" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Have you ever been Scammed? And what kind of scam?</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="haveEverBeenScammed" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="haveEverBeenScammed" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Have you ever engaged in a dispute with a purchaser regarding the well-being of a kitten?</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="haveEverEngagedInADispute" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="haveEverEngagedInADispute" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>What has been the most challenging experience you have faced to manage the cattery?</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="mostChallengingExperienceToManageTheCattery" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="mostChallengingExperienceToManageTheCattery" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>What is the most important data you would like to collect from your customers?</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="mostImportantDataWouldLiketCollectFromCustomer" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="mostImportantDataWouldLiketCollectFromCustomer" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>What is the most important thing you would like your customers to know?</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="mostImportantThingWouldLIkeCustomersToKnow" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="mostImportantThingWouldLIkeCustomersToKnow" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>What type of data would assist you in improving the management of the cattery?</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="typeOfDataWouldImprovingTheManagementOfTheCattery" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="typeOfDataWouldImprovingTheManagementOfTheCattery" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>List all the websites/apps you used to post your kittens (separate by comma)</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="websitesAppsUsedToPostKittens" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="websitesAppsUsedToPostKittens" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Have any of your breeding cats or their offspring received titles or awards? If yes, please provide details.</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="haveCatsReceivedTitlesOrAwards" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="haveCatsReceivedTitlesOrAwards" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>What health testing do you conduct on your breeding cats? Please specify the tests performed and any certifications obtained.</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="healthTestingConductOnCats" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="healthTestingConductOnCats" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Do you have a contract or agreement for buyers? If yes, please provide a brief description of the terms and conditions.</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="contactOrAgreementForBuyers" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="contactOrAgreementForBuyers" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>Do you offer any support or assistance to kitten buyers after they take their kittens home? If yes, please provide details.</Label>
 						</Form.Label>
-						<Form.Control as="textarea" rows={3} name="anySuportOrAssistanceToBuyer" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control as="textarea" rows={3} name="anySuportOrAssistanceToBuyer" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 					<Form.Group className="mb-3">
 						<Form.Label>
 							<Label>How many breeding cats do you currently own?</Label>
 						</Form.Label>
-						<Form.Control name="breedingCatsCurrentlyOwn" placeholder="" required={false} onChange={handleOnChange}/>
+						<Form.Control name="breedingCatsCurrentlyOwn" placeholder="" required={true} onChange={handleOnChange}/>
 					</Form.Group>
 
 					<hr/>
