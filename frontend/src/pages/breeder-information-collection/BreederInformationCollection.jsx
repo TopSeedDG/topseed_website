cce30c139839ce7a28ba1fcecafc2c454950e0cb
Jiaqi <ejade2816@gmail.com>
2023-07-19 23:53:37 -0700

    cors

--- frontend/src/pages/breeder-information-collection/BreederInformationCollection.jsx
+++ frontend/src/pages/breeder-information-collection/BreederInformationCollection.jsx
@@ -15,11 +15,7 @@ function BreederInformationCollection() {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log(data);
-		axios.post("https://topseed-django-development.up.railway.app/api/breeder-info-collection", data, {
-			headers: {
-			  'Access-Control-Allow-Origin': '*'
-			}
-		})
+		axios.post("https://topseed-django-development.up.railway.app/api/breeder-info-collection/", data)
 		.then(res => {
 			console.log(res.data);
 			navigate("/breeder-information-collection-submitted")
